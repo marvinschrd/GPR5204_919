84a7461ccd597be91bf0a96e12567686f90595f9
William <williamp10022000@gmail.com>
2021-02-23 19:42:34 +0100

    Adding Rotation functions to Vector2f and their tests

--- include/maths/vector2.h
+++ include/maths/vector2.h
@@ -1,5 +1,3 @@
-#pragma once
-
 /*
 MIT License
 
@@ -23,80 +21,116 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include <cmath>
+
+#pragma once
+
 #include "maths/vector3.h"
 #include "maths/angle.h"
 
 namespace maths
 {
-	/**
-	* \brief Class used to represent a 2D vector.
-	*/
-	struct Vector2f
+/**
+* \brief Class used to represent a 2D vector.
+*/
+class Vector2f
+{
+public:
+
+	union
 	{
-	public:
-		union
+		struct
 		{
-			struct
-			{
-				float x;
-				float y;
-			};
-			float coord[2]{};
+			float x;
+			float y;
 		};
 
-		Vector2f() : x(0), y(0) {}
-		
-		Vector2f(float x, float y);
-		
-		Vector2f operator+(const Vector2f rhs) const { return Vector2f(x + rhs.x, y + rhs.y); }
-		
-		Vector2f operator+=(Vector2f rhs);
-		
-		Vector2f operator-(const Vector2f rhs) const { return Vector2f(x - rhs.x, y - rhs.y); }
-
-		Vector2f operator-=(Vector2f rhs);
-
-		Vector2f operator*(const float scalar) const { return Vector2f(x * scalar, y * scalar); }
-
-		Vector2f operator*=(float scalar);
-
-		Vector2f operator/(const float scalar) const { return Vector2f(x / scalar, y / scalar); }
-
-		Vector2f operator/=(float scalar);
-
-		bool operator==(const Vector2f rhs) const { return x == rhs.x && y == rhs.y; }
-
-		bool operator!=(const Vector2f rhs) const { return x != rhs.x || y != rhs.y; }
-
-		const float& operator[](size_t component) const { return coord[component]; }
-
-		float& operator[](size_t component) { return coord[component]; }
-
-		float Magnitude() const { return float(std::sqrt(x * x + y * y)); }
-
-		float SqrMagnitude() const { return float((x * x + y * y)); }
-
-		float Dot(const Vector2f v2) const { return float(x * v2.x + y * v2.y); }
-
-		static float Dot(const Vector2f v1, const Vector2f v2) { return float(v1.x * v2.x + v1.y * v2.y); }
-
-		Vector3f Cross(const Vector2f v2) const;
-
-		static Vector3f Cross(const Vector2f v1, const Vector2f v2);
-
-		radian_t AngleBetween(const Vector2f v2) const;
-
-		static radian_t AngleBetween(const Vector2f v1, const Vector2f v2);
-
-		Vector2f Normalized() const;
-
-		void Normalize();
-
-		Vector2f Lerp(const Vector2f v2, const float t) const { return *this + (v2 - *this) * t; }
-
-		static Vector2f Lerp(const Vector2f v1, const Vector2f v2, const float t) { return  v1 + (v2 - v1) * t; }
-
-		Vector2f Slerp(Vector2f v2, const float t) const;
+		float coord[2]{};
 	};
-}
+
+	Vector2f() = default;
+
+	Vector2f(const float x, const float y) : x(x), y(y) {}
+
+	Vector2f operator+(const Vector2f rhs) const
+	{
+		return Vector2f(x + rhs.x, y + rhs.y);
+	}
+
+	Vector2f& operator+=(Vector2f rhs);
+
+	Vector2f operator-(const Vector2f rhs) const
+	{
+		return Vector2f(x - rhs.x, y - rhs.y);
+	}
+
+	Vector2f& operator-=(Vector2f rhs);
+
+	Vector2f operator*(const float scalar) const
+	{
+		return Vector2f(x * scalar, y * scalar);
+	}
+
+	Vector2f& operator*=(float scalar);
+
+	Vector2f operator/(const float scalar) const
+	{
+		return Vector2f(x / scalar, y / scalar);
+	}
+
+	Vector2f& operator/=(float scalar);
+
+	bool operator==(const Vector2f rhs) const
+	{
+		return x == rhs.x && y == rhs.y;
+	}
+
+	bool operator!=(const Vector2f rhs) const
+	{
+		return x != rhs.x || y != rhs.y;
+	}
+
+	const float operator[](const size_t component) const
+	{
+		return coord[component];
+	}
+
+	float& operator[](const size_t component) { return coord[component]; }
+
+	float Magnitude() const { return std::sqrt(x * x + y * y); }
+
+	float SqrMagnitude() const { return x * x + y * y; }
+
+	float Dot(const Vector2f v2) const { return x * v2.x + y * v2.y; }
+
+	static float Dot(const Vector2f v1, const Vector2f v2)
+	{
+		return v1.x * v2.x + v1.y * v2.y;
+	}
+
+	Vector3f Cross(Vector2f vec2) const;
+
+	static Vector3f Cross(Vector2f vec1, Vector2f vec2);
+
+	radian_t AngleBetween(Vector2f v2) const;
+
+	static radian_t AngleBetween(Vector2f v1, Vector2f v2);
+
+	Vector2f Normalized() const;
+
+	void Normalize();
+
+	Vector2f Lerp(const Vector2f v2, const float t) const
+	{
+		return *this + (v2 - *this) * t;
+	}
+
+	static Vector2f Lerp(const Vector2f v1, const Vector2f v2,
+	                     const float t) { return v1 + (v2 - v1) * t; }
+
+	Vector2f Slerp(Vector2f v2, float t) const;
+
+	Vector2f Rotation(radian_t angle) const;
+
+	static Vector2f Rotation(Vector2f v1, radian_t angle);
+};
+} // namespace maths
--- test/test_vector2.cpp
+++ test/test_vector2.cpp
@@ -25,17 +25,19 @@ SOFTWARE.
 #include <gtest/gtest.h>
 #include "maths/vector2.h"
 
+namespace tests
+{
 TEST(Maths, Vector2f_Addition)
 {
 	const maths::Vector2f a{2.0f, 3.0f};
 	const maths::Vector2f b{1.0f, 4.0f};
 
-	//Test operator +
+	//Test operator + .
 	maths::Vector2f c = a + b;
 	EXPECT_EQ(c.x, a.x + b.x);
 	EXPECT_EQ(c.y, a.y + b.y);
 
-	//Test operator +=
+	//Test operator += .
 	const maths::Vector2f d{3.0f, 4.0f};
 	maths::Vector2f e = d;
 	e += a;
@@ -48,12 +50,12 @@ TEST(Maths, Vector2f_Substraction)
 	const maths::Vector2f a{2.0f, 3.0f};
 	const maths::Vector2f b{1.0f, 4.0f};
 
-	//Test operator -
+	//Test operator - .
 	maths::Vector2f c = a - b;
 	EXPECT_EQ(c.x, a.x - b.x);
 	EXPECT_EQ(c.y, a.y - b.y);
 
-	//Test operator -=
+	//Test operator -= .
 	const maths::Vector2f d{3.0f, 4.0f};
 	maths::Vector2f e = d;
 	e -= a;
@@ -66,12 +68,12 @@ TEST(Maths, Vector2f_MultiplicationByScalar)
 	const maths::Vector2f a{2.0f, 3.0f};
 	const float b = 4.0f;
 
-	//Test operator +
+	//Test operator + .
 	maths::Vector2f c = a * b;
 	EXPECT_EQ(c.x, a.x * b);
 	EXPECT_EQ(c.y, a.y * b);
 
-	//Test operator +=
+	//Test operator += .
 	const maths::Vector2f d{3.0f, 4.0f};
 	maths::Vector2f e = d;
 	e *= b;
@@ -84,12 +86,12 @@ TEST(Maths, Vector2f_DivisionByScalar)
 	const maths::Vector2f a{2.0f, 3.0f};
 	const float b = 4.0f;
 
-	//Test operator /
+	//Test operator / .
 	maths::Vector2f c = a / b;
 	EXPECT_EQ(c.x, a.x / b);
 	EXPECT_EQ(c.y, a.y / b);
 
-	//Test operator /=
+	//Test operator /= .
 	const maths::Vector2f d{3.0f, 4.0f};
 	maths::Vector2f e = d;
 	e /= b;
@@ -99,55 +101,55 @@ TEST(Maths, Vector2f_DivisionByScalar)
 
 TEST(Maths, Vector2f_Magnitude)
 {
-	const maths::Vector2f a{ 2.0f, 3.0f };
+	const maths::Vector2f a{2.0f, 3.0f};
 	const float b = 4;
 
-	//Test .Magnitude()
+	//Test .Magnitude() .
 	EXPECT_EQ(a.Magnitude(), std::sqrt((a.x * a.x) + (a.y * a.y)));
 }
 
 TEST(Maths, Vector2f_SqrMagnitude)
 {
-	const maths::Vector2f a{ 2.0f, 3.0f };
+	const maths::Vector2f a{2.0f, 3.0f};
 	const float b = 4;
 
-	//Test .SqrMagnitude()
+	//Test .SqrMagnitude() .
 	EXPECT_EQ(a.SqrMagnitude(), (a.x * a.x) + (a.y * a.y));
 }
 
 TEST(Maths, Vector2f_DotProduct)
 {
-	const maths::Vector2f a{ 0.0f, 3.0f };
-	const maths::Vector2f b{ 1.0f, 0.0f };
+	const maths::Vector2f a{0.0f, 3.0f};
+	const maths::Vector2f b{1.0f, 0.0f};
 
-	//Test .Dot()
+	//Test .Dot() .
 	EXPECT_EQ(a.Dot(b), 0.0f);
 
-	//Test static Dot()
+	//Test static Dot() .
 	EXPECT_EQ(maths::Vector2f::Dot(a, b), 0.0f);
 
 
-	const maths::Vector2f c{ 2.0f, 3.0f };
-	const maths::Vector2f d{ 1.0f, 1.0f };
+	const maths::Vector2f c{2.0f, 3.0f};
+	const maths::Vector2f d{1.0f, 1.0f};
 
-	//Test .Dot()
+	//Test .Dot() .
 	EXPECT_EQ(c.Dot(d), c.x * d.x + c.y * d.y);
 
-	//Test static Dot()
+	//Test static Dot() .
 	EXPECT_EQ(maths::Vector2f::Dot(c, d), c.x * d.x + c.y * d.y);
 }
 
 TEST(Maths, Vector2f_CrossProduct)
 {
-	const maths::Vector2f a{ 2.0f, 3.0f };
-	const maths::Vector2f b{ 1.0f, -3.0f };
+	const maths::Vector2f a{2.0f, 3.0f};
+	const maths::Vector2f b{1.0f, -3.0f};
 
-	//Test .Cross()
+	//Test .Cross() .
 	EXPECT_EQ(a.Cross(b).x, 0);
 	EXPECT_EQ(a.Cross(b).y, 0);
 	EXPECT_EQ(a.Cross(b).z, a.x * b.y - a.y * b.x);
 
-	//Test static Cross()
+	//Test static Cross() .
 	EXPECT_EQ(maths::Vector2f::Cross(a, b).x, 0);
 	EXPECT_EQ(maths::Vector2f::Cross(a, b).y, 0);
 	EXPECT_EQ(maths::Vector2f::Cross(a, b).z, a.x * b.y - a.y * b.x);
@@ -155,25 +157,28 @@ TEST(Maths, Vector2f_CrossProduct)
 
 TEST(Maths, Vector2f_AngleBetween)
 {
-	const maths::Vector2f a{ 0.0f, 3.0f };
-	const maths::Vector2f b{ 1.0f, 0.0f };
+	const maths::Vector2f a{0.0f, 3.0f};
+	const maths::Vector2f b{1.0f, 0.0f};
 
-	//Test .AngleBetween()
-	EXPECT_EQ(a.AngleBetween(b), maths::acos(a.Dot(b) / (a.Magnitude() * b.Magnitude())));
+	//Test .AngleBetween() .
+	EXPECT_EQ(a.AngleBetween(b),
+	          maths::acos(a.Dot(b) / (a.Magnitude() * b.Magnitude())));
 
-	//Test static AngleBetween()
-	EXPECT_EQ(maths::Vector2f::AngleBetween(a, b), maths::acos(maths::Vector2f::Dot(a, b) / (a.Magnitude() * b.Magnitude())));
+	//Test static AngleBetween() .
+	EXPECT_EQ(maths::Vector2f::AngleBetween(a, b),
+	          maths::acos(maths::Vector2f::Dot(a, b) / (a.Magnitude() *
+		          b.Magnitude())));
 }
 
 TEST(Maths, Vector2f_Normalize)
 {
-	const maths::Vector2f a{ 0.0f, 3.0f};
+	const maths::Vector2f a{0.0f, 3.0f};
 	const maths::Vector2f b = a.Normalized();
 
-	//Test .Normalized()
+	//Test .Normalized() .
 	EXPECT_EQ(b.Magnitude(), 1.0f);
 
-	//Test .Normalize()
+	//Test .Normalize() .
 	maths::Vector2f c = a;
 	c.Normalize();
 	EXPECT_EQ(c.Magnitude(), 1.0f);
@@ -181,28 +186,28 @@ TEST(Maths, Vector2f_Normalize)
 
 TEST(Maths, Vector2f_Lerp)
 {
-	const maths::Vector2f a{ 2.0f, 3.0f};
-	const maths::Vector2f b{ 1.0f, 4.0f};
+	const maths::Vector2f a{2.0f, 3.0f};
+	const maths::Vector2f b{1.0f, 4.0f};
 
 	const float t0 = 0.0f;
 	const float t1 = 1.0f;
 
-	//Test static Vector2f Lerp t = 0
+	//Test static Vector2f Lerp t = 0.
 	maths::Vector2f c = maths::Vector2f::Lerp(a, b, t0);
 	EXPECT_EQ(c.x, a.x);
 	EXPECT_EQ(c.y, a.y);
 
-	//Test static Vector2f Lerp t = 1
+	//Test static Vector2f Lerp t = 1.
 	maths::Vector2f d = maths::Vector2f::Lerp(a, b, t1);
 	EXPECT_EQ(d.x, b.x);
 	EXPECT_EQ(d.y, b.y);
 
-	//Test Vector2f Lerp t = 0
+	//Test Vector2f Lerp t = 0.
 	maths::Vector2f e = a.Lerp(b, t0);
 	EXPECT_EQ(e.x, a.x);
 	EXPECT_EQ(e.y, a.y);
 
-	//Test Vector2f Lerp t = 1
+	//Test Vector2f Lerp t = 1.
 	maths::Vector2f f = a.Lerp(b, t1);
 	EXPECT_EQ(f.x, b.x);
 	EXPECT_EQ(f.y, b.y);
@@ -211,22 +216,42 @@ TEST(Maths, Vector2f_Lerp)
 TEST(Maths, Vector2f_Slerp)
 {
 	float threshold = 0.0001f;
-
-	const maths::Vector2f a{ 2.0f, 3.0f};
-	const maths::Vector2f b{ 1.0f, 4.0f};
+	
+	const maths::Vector2f a{2.0f, 3.0f};
+	const maths::Vector2f b{1.0f, 4.0f};
 
 	const float t0 = 0.0f;
 	const float t1 = 1.0f;
 
-	//Test Vector2f Slerp t = 0
+	//Test Vector2f Slerp t = 0.
 	maths::Vector2f c = a.Slerp(b, t0);
 	//Check difference between result & expected value
-	//Because slerp function lose too much precision with double
+	//Because slerp function lose too much precision with double.
 	EXPECT_TRUE(std::abs(c.x - a.x) < threshold);
 	EXPECT_TRUE(std::abs(c.y - a.y) < threshold);
 
-	//Test Vector2f Slerp t = 1
+	//Test Vector2f Slerp t = 1.
 	maths::Vector2f d = a.Slerp(b, t1);
 	EXPECT_TRUE(std::abs(d.x - b.x) < threshold);
 	EXPECT_TRUE(std::abs(d.y - b.y) < threshold);
 }
+
+TEST(Maths, Vector2f_Rotation)
+{
+	const maths::Vector2f a = maths::Vector2f{1.0f, 3.0f};
+	const maths::degree_t b = maths::degree_t(45.f);
+	const maths::radian_t c = b;
+
+	//Test .Rotation() .
+	EXPECT_EQ(a.Rotation(b).x,
+	          (a.x * maths::cos(c)) + (a.y * -maths::sin(c)));
+	EXPECT_EQ(a.Rotation(b).y,
+	          (a.x * maths::sin(c)) + (a.y * maths::cos(c)));
+
+	//Test static Rotation() .
+	EXPECT_EQ(maths::Vector2f::Rotation(a, b).x,
+	          (a.x * maths::cos(c)) + (a.y * -maths::sin(c)));
+	EXPECT_EQ(maths::Vector2f::Rotation(a, b).y,
+	          (a.x * maths::sin(c)) + (a.y * maths::cos(c)));
+}
+} // namespace tests
